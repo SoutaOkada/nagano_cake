72c5517f07e2a65d11e8fba12771e321f2ee1c0e
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2022-03-20 14:55:02 +0000

    [add] add host pemission
    add host permission to config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "4b8b77dcbbc74a78ab3a28c3854a4a86.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
