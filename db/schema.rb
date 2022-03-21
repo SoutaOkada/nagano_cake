5ccc3ecbc3e79420c1d675f64a0ff815617210e3
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2022-03-21 14:13:15 +0000

    [add] add address column
    add colomns to addresses migration file
    execute db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_143259) do
+ActiveRecord::Schema.define(version: 2022_03_21_140254) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
