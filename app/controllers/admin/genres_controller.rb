accec2b8979c4021d2a7726e9d395754d3430309
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2022-03-24 13:38:22 +0000

    [add] genres controller & views
    execute rails g controller admin/genres index create edit update
    fix route sentences to resources style
    fix index.html message
    add header to genres controller

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,16 @@
+class Admin::GenresController < ApplicationController
+  layout 'admin'
+
+  def index
+  end
+
+  def create
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
   devise_for :admin, skip: [:passwords] ,controllers: {
     sessions: "admin/sessions",
     registrations: "admin/registrations"
