979c8c61e25b1152c133e6b0cdb6d0763d389743
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2022-03-24 12:30:46 +0000

    [add] install jQuery
    install jquery

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get '/' => "homes#top"
-  end
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
+  devise_for :admin, skip: [:passwords] ,controllers: {
+    sessions: "admin/sessions",
+    registrations: "admin/registrations"
   }
 
   devise_for :customer, skip: [:passwords] ,controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  namespace :admin do
+    root to: 'homes#top'
+    # get 'admin/sign_out' => 'devise/sessions#destroy'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
