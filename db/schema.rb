af53ece4366c971fa5dab0318733b38eabcc851a
EC2 Default User <ec2-user@ip-172-31-95-155.ec2.internal>
2022-03-28 13:25:19 +0000

    [update] set items/is_active deafult value
    update items/is_active value nil to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_112450) do
+ActiveRecord::Schema.define(version: 2022_03_28_131920) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_112450) do
     t.string "name"
     t.text "introduction"
     t.integer "price"
-    t.boolean "is_active"
+    t.boolean "is_active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "genre_id"
