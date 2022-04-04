99a0c214872dee534c07272f7b9e307e3e661daa
SoutaOkada <soscmkao0926@gmail.com>
2022-04-04 15:45:03 +0000

    [update] mount edit customer button
    edit customer/show.html & customers controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,6 +8,9 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customer_path(current_customer.id)
   end
 
   def confirm
@@ -15,4 +18,12 @@ class Public::CustomersController < ApplicationController
 
   def withdrawal
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana,\
+      :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
