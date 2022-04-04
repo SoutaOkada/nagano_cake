4f1de801a86c97bb79eb23bce741c01c9ffbf024
SoutaOkada <soscmkao0926@gmail.com>
2022-04-04 04:34:10 +0000

    [update] customer registration & session feature
    add sentence for header in public.html
    transfer after sign out path from application controller to admin/homes_controller
    add after sign out path in public/homes controller

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,8 @@
 class Admin::HomesController < ApplicationController
+    def after_sign_out_path_for(resource)
+        admin_session_path
+    end
+
   layout 'admin'
 
   def top
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,11 @@
 class Public::HomesController < ApplicationController
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   layout 'public'
 
   def top
-
   end
 
   def about
