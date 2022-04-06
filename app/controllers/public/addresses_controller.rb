b42d09c74b58561ceb85efd51893562c1d189ae5
SoutaOkada <soscmkao0926@gmail.com>
2022-04-06 12:37:31 +0000

    [update] update addresses post & index feature
    execute rails g controller public/addresses
    edit html & controller files

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,28 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.where(@address.customer_id == current_customer.id)
   end
 
   def edit
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to request.referer
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
