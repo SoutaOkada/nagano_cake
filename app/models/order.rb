968495907d35af8679a3e4d1c8e6f0646b310ced
SoutaOkada <soscmkao0926@gmail.com>
2022-04-14 12:04:47 +0000

    [update] update enum in order model
    edit order.rb & ja.yml file

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :order_detail
   belongs_to :customer
-  
+
   enum payment_method: {credit_card: 0, transfer: 1}
+  enum status: {waiting_pay: 0, payed: 1, progress: 2, standby: 3, complete: 4}
 end
