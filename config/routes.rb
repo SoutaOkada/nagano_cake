947c410ba972acd0085ae35b1d9c4cc98e8d40c8
SoutaOkada <soscmkao0926@gmail.com>
2022-04-20 17:34:22 +0000

    [update] update admin order feature
    edit files & add neccesary files

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,10 @@ Rails.application.routes.draw do
 
     resources :customers, only: [:index, :show,:edit, :update]
 
+    resources :orders, only:[:show, :update]
+
+    resources :order_details, only: [:update]
+
   end
 
   devise_for :admin, skip: [:passwords] ,controllers: {
